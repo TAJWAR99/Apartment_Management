9ba82399fdc9036fc31227e799649af47103c9f3
TAJWAR99 <63014244+TAJWAR99@users.noreply.github.com>
2021-08-23 01:57:36 +0600

    Update settings.py

--- apartmentManagement/settings.py
+++ apartmentManagement/settings.py
@@ -81,14 +81,11 @@ WSGI_APPLICATION = 'apartmentManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DEMO_TEST',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
